0b213c0d4df5b9e0cc89e2f00e9aa182e98674da
HappyHomeBS <stairs13@naver.com>
2022-11-02 21:14:47 +0900

    write

--- src/main/java/com/green/controller/WriteController.java
+++ src/main/java/com/green/controller/WriteController.java
@@ -3,12 +3,14 @@ package com.green.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class WriteController {
 	
 	@GetMapping("/Write")
 	public String Write() {
-		return "write5";
+		return "write";
 	}
 
 }
