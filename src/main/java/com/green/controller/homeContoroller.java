be99dd35fd662026493549cff56685cdc5ae5155
sonheelyong <gmlfyd417@gmail.com>
2022-11-02 20:40:19 +0900

    메시지바꿈

--- src/main/java/com/green/controller/homeContoroller.java
+++ src/main/java/com/green/controller/homeContoroller.java
@@ -14,7 +14,7 @@ public class homeContoroller {
 	      
 	   }
 	   @RequestMapping("/RecepMessage")
-	   public String recepMessage() {
+	   public String recepMessage2() {
 	      
 	      return "/recepMessage";
 	   
